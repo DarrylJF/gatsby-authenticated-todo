2937ca9bb84512b4f0691bc08b0067d677f967a8
DJF <darryljf.develop@gmail.com>
2021-04-20 12:49:06 +0100

    Added DeleteIcon to list item and moved onClick function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 const TodoList = ({todos, onRemoveTodo}) => {
   
@@ -7,10 +9,14 @@ const TodoList = ({todos, onRemoveTodo}) => {
       <h2>Loaded Todo List</h2>
       <ul>
         {todos.map(todo => (
-          <li key={todo.id} onClick={() => onRemoveTodo(todo.id)} style={{border: "1px solid black", marginBottom: "5px", cursor: "pointer"}}>
+          <li key={todo.id} style={{border: "1px solid black", marginBottom: "5px", cursor: "pointer"}}>
             <span>{todo.name}</span>
             <span>{todo.todo}</span>
             <span>{todo.priority}</span>
+            <span>
+              <DeleteIcon onClick={() => onRemoveTodo(todo.id)}/>
+              <EditIcon/>
+            </span>
           </li>
         ))}
       </ul>
